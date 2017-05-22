757cfd520368a84eda3ec6ec84143d8da60104db
Bailey Kursar <bkursar@gmail.com>
2017-05-22 17:42:17 +0100

    Added a note list and note list array

--- galaxyTestFrameworkV2.js
+++ galaxyTestFrameworkV2.js
@@ -1,8 +1,9 @@
 'use strict';
 
+// This tests one note
 (function(exports){
-var string = "My favourite testing framework is Galaxy(tm)";
-var note = new Note(string)
+  var string = "My favourite testing framework is Galaxy(tm)";
+  var note = new Note(string);
 
   function firstTest() {
     var result = (note.text === string);
@@ -19,6 +20,20 @@ var note = new Note(string)
 
 })(this);
 
+// This tests the list of notes
+(function(exports){
+  var noteList = new NoteList();
+
+  function thirdTest() {
+    var result = Array.isArray(noteList.notes);
+    return ('Note list has an array of notes: ' + result);
+  }
+
+  exports.thirdTest = thirdTest;
+  
+})(this);
+
 console.log('%c-----GALAXY TEST-----', 'background: black; color: white; display: block;');
 console.log(firstTest());
 console.log(secondTest());
+console.log(thirdTest());
---
+++ note-list-model.js
@@ -0,0 +1,5 @@
+'use strict';
+
+function NoteList() {
+  this.notes = [];
+}
