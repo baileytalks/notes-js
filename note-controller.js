3ed43e2075a394460c89838a079a1dfc2fe6adc1
Bailey Kursar <bkursar@gmail.com>
2017-05-25 15:20:17 +0100

    Spiked the note-controller to be able to instantiate a notecontroller with text, create a notelist with the text, then print the html with .createHTML

--- note-controller.js
+++ note-controller.js
@@ -1,13 +1,16 @@
 "use-strict";
 
-// var NoteList = require('./models/note-list-model.js')
-//
 // const changeText = function(text) {
 //     var appDiv = document.getElementById('app');
 //     appDiv.innerHTML = text;
 // };
 
-// function NoteController(text) {
-//   var note = new NoteList;
-//   var note = note.newNote('text');
-// };
+function NoteController(text) {
+  var noteList = new NoteList();
+  var note = noteList.addNote(text);
+  var noteListView = new NoteListView();
+
+  NoteController.prototype.createHTML = function() {
+    return noteListView.createHTML(noteList);
+  }
+};
