901d8006d6db97cfc31d5e9fdf92ef86ce0ef6ec
Bailey Kursar <bkursar@gmail.com>
2017-05-26 15:23:46 +0100

    Added a single note view and a test

--- models/single-note-view.js
+++ models/single-note-view.js
@@ -1,1 +1,10 @@
-function ()
+(function(exports) {
+  function SingleNoteView(notemodel) {
+    this.createHTML = '<div>' + notemodel.text + '</div>';
+
+    SingleNoteView.prototype.noteview = function() {
+      return this.createHTML;
+    }
+  }
+  exports.SingleNoteView = SingleNoteView;
+})(this);
--- tests/baileyTests.js
+++ tests/baileyTests.js
@@ -1,4 +1,4 @@
-(function(exports){
+(function() {
 
   function DummyNoteController() {
     var pageHTML = function(text) {
@@ -17,36 +17,49 @@
     }
   };
 
-  var string = "My favourite testing framework is this one",
-      note = new Note('string'),
-      noteList = new NoteList();
-      noteListView = new NoteListView();
-      dummyNoteController = new DummyNoteController();
+    var note = new Note('string');
+    var noteList = new NoteList();
+    var noteListView = new NoteListView();
+    var dummyNoteController = new DummyNoteController();
+    var singleNoteView = new SingleNoteView(note);
 
-    elementsAreTheSame('Contains a string', note.text, 'string')
-    elementsAreTheSame('Using .text prints the string', note.textPrint(), 'string')
+    var title = 'Contains a string';
+    elementsAreTheSame(title, note.text, 'string')
 
+    var title = 'Using .text prints the string';
+    elementsAreTheSame(title, note.textPrint(), 'string')
+
+    var title = 'Making new note pushes to array';
     var note1 = { text: 'Here is a note'};
     noteList.addNote('fakenote', note1);
-    elementsAreTheSame('Making new note pushes to array', noteList.notes[0], note1)
+    elementsAreTheSame(title, noteList.notes[0], note1)
 
+    var title = 'Displays all notes in list';
     noteList.addNote('fakenote', { text: 'Here is another note' });
-    elementsAreTheSame('Displays all notes in list', noteList.displayNotes(), "Here is a note, Here is another note")
+    elementsAreTheSame(title, noteList.displayNotes(), "Here is a note, Here is another note")
 
+    var title = 'Produces string of HTML with notes';
     var htmlString = '<ul><li><div>Here is a note</div></li><li><div>Here is another note</div></li></ul>';
     var fakeNoteList = { notes: [{ text: 'Here is a note' }, { text: 'Here is another note' }] };
-    elementsAreTheSame('Produces string of HTML with notes', noteListView.createHTML(fakeNoteList), htmlString);
+    elementsAreTheSame(title, noteListView.createHTML(fakeNoteList), htmlString);
 
+    var title = 'Produces string of HTML with one note';
     var htmlStringOneNote = '<ul><li><div>Here is a note</div></li></ul>';
     var fakeNoteObject = { notes: [{ text: 'Here is a note' }] };
-    elementsAreTheSame('Produces string of HTML with one note', noteListView.createHTML(fakeNoteObject), htmlStringOneNote);
+    elementsAreTheSame(title, noteListView.createHTML(fakeNoteObject), htmlStringOneNote);
 
+    var title = 'Does not produce string of HTML with no note';
     var htmlStringNoNotes = '';
     var fakeNoNote = [];
-    elementsAreTheSame('Does not produce string of HTML with no note', noteListView.createHTML(fakeNoNote), htmlStringNoNotes);
+    elementsAreTheSame(title, noteListView.createHTML(fakeNoNote), htmlStringNoNotes);
 
+    var title = 'The innerHTML property of the app element contains HTML with string';
     dummyNoteController.pageHTML('Favourite food: pesto');
     var appDiv = document.getElementById('app').innerHTML;
     var foodNote = 'Favourite food: pesto';
-    elementContains('The innerHTML property of the app element contains HTML with string', appDiv, foodNote);
+    elementContains(title, appDiv, foodNote);
+
+    var title = 'Expects the output of single note view to include the note given to it';
+    elementContains(title, singleNoteView.noteview(), 'string')
+
 })();
